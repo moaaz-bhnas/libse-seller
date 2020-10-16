ca7b48688009259e140ead5b7857cede68389348
moaaz-bhnas <moaaz_bs@yahoo.com>
2020-10-16 22:48:41 +0200

    redirection issue

--- src/Protected.js
+++ src/Protected.js
@@ -1,14 +1,15 @@
 import { useRouter } from "next/router";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { AuthContext } from "./contexts/auth";
 import { LocaleContext } from "./contexts/locale";
+import useUpdateEffect from "./hooks/useUpdateEffect";
 
 const Protected = ({ children }) => {
   const { user } = useContext(AuthContext);
   const { locale } = useContext(LocaleContext);
 
   const router = useRouter();
-  useEffect(() => {
+  useUpdateEffect(() => {
     if (!user) {
       router.push(`/${locale}/login`);
     }
