4fddf7cd8b0a35a19a6d5f952370d435b7de79a9
moaaz-bhnas <moaaz_bs@yahoo.com>
2021-01-04 03:23:43 +0200

    fixed canvas images quality

--- src/components/AddProductForm/components/ImageCropperModal.js
+++ src/components/AddProductForm/components/ImageCropperModal.js
@@ -125,6 +125,7 @@ const ImageCropperModal = ({ src, setSrc, imageInputRef, addImage }) => {
       crop,
       "product-image.jpeg"
     );
+    console.log("crop: ", crop, "croppedImage: ", croppedImage);
     addImage(croppedImage);
     setSrc(null);
     imageInputRef.current.focus();
@@ -134,9 +135,18 @@ const ImageCropperModal = ({ src, setSrc, imageInputRef, addImage }) => {
     const canvas = document.createElement("canvas");
     const scaleX = image.naturalWidth / image.width;
     const scaleY = image.naturalHeight / image.height;
-    canvas.width = crop.width;
-    canvas.height = crop.height;
+    canvas.width = crop.width * scaleX;
+    canvas.height = crop.height * scaleY;
     const ctx = canvas.getContext("2d");
+
+    console.log(
+      "crop.width: ",
+      crop.width,
+      "scaleX: ",
+      scaleX,
+      "crop.width * scaleX: ",
+      crop.width * scaleX
+    );
 
     ctx.drawImage(
       image,
@@ -146,21 +156,25 @@ const ImageCropperModal = ({ src, setSrc, imageInputRef, addImage }) => {
       crop.height * scaleY,
       0,
       0,
-      crop.width,
-      crop.height
+      crop.width * scaleX,
+      crop.height * scaleY
     );
 
     return new Promise((resolve, reject) => {
-      canvas.toBlob((blob) => {
-        if (!blob) {
-          console.error("Canvas is empty");
-          return;
-        }
-        blob.name = fileName;
-        window.URL.revokeObjectURL(url.current);
-        url.current = window.URL.createObjectURL(blob);
-        resolve({ file: blob, url: url.current });
-      }, "image/jpeg");
+      canvas.toBlob(
+        (blob) => {
+          if (!blob) {
+            console.error("Canvas is empty");
+            return;
+          }
+          blob.name = fileName;
+          window.URL.revokeObjectURL(url.current);
+          url.current = window.URL.createObjectURL(blob);
+          resolve({ file: blob, url: url.current });
+        },
+        "image/png",
+        1
+      );
     });
   }, []);
 
--- src/components/gallery/components/Images.js
+++ src/components/gallery/components/Images.js
@@ -1,11 +1,25 @@
 import { memo } from "react";
 import styled from "styled-components";
+import { listStyles } from "../../List/style";
+import Image from "next/image";
 
 const Images = ({ images }) => {
   console.log(images);
-  return <StyledImages></StyledImages>;
+  return (
+    <StyledImages>
+      {images.map((image, index) => (
+        <Item key={index}>
+          <Image src={image} alt="" layout="fill" unsized />
+        </Item>
+      ))}
+    </StyledImages>
+  );
 };
 
-const StyledImages = styled.ul``;
+const StyledImages = styled.ul`
+  ${listStyles}
+`;
+
+const Item = styled.li``;
 
 export default memo(Images);
